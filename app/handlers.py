619c08ce4cd99dfca61be3e16f1c77220ae4841b
Sergio Hidalgo <serghidalg@riseup.net>
2025-12-30 15:50:28 +0100

    changing tag telegram to username

--- app/handlers.py
+++ app/handlers.py
@@ -72,7 +72,7 @@ async def create_user_handler(update: Update, context: ContextTypes.DEFAULT_TYPE
         password=password, 
         first_name=first_name, 
         last_name=last_name, 
-        telegram_id=telegram_id_value # Usamos el ID numérico
+        telegram_id=telegram_user_name # Usamos el ID numérico
     )
     
     if not success:
